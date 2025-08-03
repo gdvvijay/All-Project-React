e4a92f08a8ee781ce07978bd35cb01f4d23a34b2
GdvVijay <gadhavivijay2021@gmail.com>
2025-08-03 16:30:36 +0530

    all images updated in shoe company

--- 15_Shoe-Company/components/Footer.jsx
+++ 15_Shoe-Company/components/Footer.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import logo1 from '../src/assets/logo1.png'
 
 export default function Footer(){
     return(
         <footer className="section-container">
             <div className="flex items-center justify-center">
                 <Link to="#">
-                    <img className="w-20" src="../src/assets/logo1.png" alt="logo"/>
+                    <img className="w-20" src={logo1} alt="logo"/>
 
                 </Link>
                 <p className="">Made with 🤍 By Vijay gadhvi The Shoe Company</p>
--- 15_Shoe-Company/components/HowItWork.jsx
+++ 15_Shoe-Company/components/HowItWork.jsx
@@ -1,16 +1,22 @@
+import jutti2 from '../src/assets/jutti2.png'
+import arrow1 from '../src/assets/arrow-1.png'
+import arrow2 from '../src/assets/arrow-2.png'
+import arrow3 from '../src/assets/arrow-3.png'
+
 export default function HowItWork(){
+
     return(
         <section id="how-it-work" className="section-container pt-22">
             <h2 className="text-center text-3xl font-bold max-w-md mx-auto pt-12 mb-12">Have they finally made the perfect workout shoe?</h2>
             <div className="max-w-2xl mx-auto pt-72 xl:pt-12 relative">
-                <img className="w-full max-w-2xl rotate-6 p-8 @min-[440px]:p-0 xl:rotate-0" src="../src/assets/jutti2.png" alt="second shoe"/>
+                <img className="w-full max-w-2xl rotate-6 p-8 @min-[440px]:p-0 xl:rotate-0" src={jutti2} alt="second shoe"/>
                 <div className="max-w-[200px] xl:max-w-xs absolute -top-12 xl:-translate-x-full">
                     <h3 className="text-lg @min-[560px]:text-2xl font-bold">The Heel</h3>
                     <p className="text-sx @min-[560px]:text-lg">The heel is well-built, has appropriate elevation and is not too rigid.</p>
-                    <img className="w-32 xl:ml-auto rotate-45 xl:rotate-0 translate-y-10" src="../src/assets/arrow-1.png" alt="arrow 1"/>
+                    <img className="w-32 xl:ml-auto rotate-45 xl:rotate-0 translate-y-10" src={arrow1} alt="arrow 1"/>
                 </div>
                 <div className="max-w-[200px] xl:max-w-xs absolute top-16 @min-[560px]:top-0 right-0 xl:-right-62">
-                    <img className="w-32 @min-[560px]:w-auto xl:-ml-26 translate-y-full @min-[560px]:translate-y-60 rotate-[120deg] xl:rotate-0 xl:translate-y-0" src="../src/assets/arrow-2.png" alt="arrow 2"/>
+                    <img className="w-32 @min-[560px]:w-auto xl:-ml-26 translate-y-full @min-[560px]:translate-y-60 rotate-[120deg] xl:rotate-0 xl:translate-y-0" src={arrow2} alt="arrow 2"/>
                     <div className="ml-10">
                         <h3 className="text-lg @min-[560px]:text-2xl font-bold -mt-30">THE FRONT</h3>
                         <p className="text-sx @min-[560px]:text-lg">The front is comfortable and adjusts well to the shape of your foot.</p>
@@ -18,7 +24,7 @@ export default function HowItWork(){
 
                 </div>
                 <div className="max-w-[200px] xl:max-w-xs absolute -bottom-10">
-                    <img className="ml-auto -mb-14 max-w-[150px] pb-6" src="../src/assets/arrow-3.png" alt="arrow 3"/>
+                    <img className="ml-auto -mb-14 max-w-[150px] pb-6" src={arrow3} alt="arrow 3"/>
                     <h3 className="text-lg @min-[560px]:text-2xl font-bold">TRACTION</h3>
                     <p className="text-sx @min-[560px]:text-lg">The shoe has good traction and doesn't slip easily.</p>
                 </div>
