93894bd36bf8f12f46c81c3a1ba6b32c7954f931
GdvVijay <gadhavivijay2021@gmail.com>
2025-08-03 16:14:36 +0530

    image update in shoe company

--- 15_Shoe-Company/components/Header.jsx
+++ 15_Shoe-Company/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <header className="px-6 md:px-12 text-white flex items-center justify-between fixed w-full max-w-[1440px] left-[50%] -translate-x-[50%] z-10">
             <Link to="/">
-                <img className="w-24 md:w-32" src="../src/assets/logo1.png" alt="logo"/>
+                <img className="w-24 md:w-32" src="/src/assets/logo1.png" alt="logo"/>
             </Link>
             <nav className="hidden md:block space-x-6 lg:space-x-8 font-bold lg:text-2xl">
                 <Link to="#hero">Home</Link>
