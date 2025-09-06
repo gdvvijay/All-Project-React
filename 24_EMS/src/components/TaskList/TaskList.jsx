391237e19d660198064aa001f856a3c00ebc4480
GdvVijay <gadhavivijay2021@gmail.com>
2025-09-06 23:54:54 +0530

    new changes

--- 24_EMS/src/components/TaskList/TaskList.jsx
+++ 24_EMS/src/components/TaskList/TaskList.jsx
@@ -1,14 +1,32 @@
+import { useRef } from "react";
 import AcceptTask from "./AcceptTask";
 import CompletedTask from "./CompleteTask";
 import FailedTask from "./FailedTask";
 import NewTask from "./NewTask";
 
 export default function TaskList({data}) {
+  const scrollableDiv = useRef()
+   function scrollToLeft(){
+     scrollableDiv.current?.scrollBy({
+  left: -200,
+  top: 0, 
+  behavior: 'smooth'
+})
+   }
+    function scrollToRight(){
+     scrollableDiv.current?.scrollBy({
+  left: 200,
+  top: 0, 
+  behavior: 'smooth'
+})
+   }
   return (
     <div
       id="tasklist"
-      className="h-[55%] w-full py-7 mt-10 flex overflow-x-auto items-center justify-start gap-5 flex-nowrap"
-    >
+      className="h-[55%] w-full py-7 mt-10 flex overflow-x-auto items-center justify-start gap-5 flex-nowrap relative" ref={scrollableDiv}
+     >
+      <div onClick={scrollToLeft} className="select-none left-button fixed cursor-pointer rounded-full border-3 h-14 w-14 border-white !text-white text-3xl z-10  flex pt-1 left-1">{`<--`}</div>
+      <div onClick={scrollToRight} className="select-none left-button fixed cursor-pointer rounded-full border-3 h-14 w-14 border-white !text-white text-3xl z-10  flex pt-1 right-1">{`-->`}</div>
         {
           data.tasks.map((elem,idx)=>{
             if(elem.active){
