56c732a47d38a4b624f0136d8a979b36e58c2e30
GdvVijay <gadhavivijay2021@gmail.com>
2025-10-17 23:54:42 +0530

    new change

--- 26_Ecommerce-Website/src/components/UseRaring.jsx
+++ 26_Ecommerce-Website/src/components/UseRaring.jsx
@@ -1,23 +1,25 @@
 import filledStar from "../assets/filledStar.png";
 import emptyStar from "../assets/emptyStar.png";
 
- export default function UseRating({item}){
-    return(
-      <>
-        {
-                    Array.from({length:Math.floor(parseInt(item.rating))}).map(()=><img key={crypto.randomUUID()}
-                  src={filledStar}
-                  alt="colored star"
-                  className=" h-full w-full"
-                />)
-                }
-               {
-                 Array.from({length:5-Math.floor(parseInt(item.rating))}).map(()=><img key={crypto.randomUUID()}
-                  src={emptyStar}
-                  alt="not colored star"
-                  className="h-full w-full"
-                />)
-               }
-      </>
-    )
-   }
+export default function UseRating({ item }) {
+  return (
+    <>
+      {Array.from({ length: Math.floor(parseInt(item.rating)) }).map(() => (
+        <img
+          key={crypto.randomUUID()}
+          src={filledStar}
+          alt="colored star"
+          className=" h-full w-full"
+        />
+      ))}
+      {Array.from({ length: 5 - Math.floor(parseInt(item.rating)) }).map(() => (
+        <img
+          key={crypto.randomUUID()}
+          src={emptyStar}
+          alt="not colored star"
+          className="h-full w-full"
+        />
+      ))}
+    </>
+  );
+}
